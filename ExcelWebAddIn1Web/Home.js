2991a63a0b7f337a78ba916130d9194395c34582
Jianxiang Lu <Seamus.Lu@kbr.com>
2023-12-04 22:40:44 +1100

    da

--- ExcelWebAddIn1Web/Home.js
+++ ExcelWebAddIn1Web/Home.js
@@ -1,7 +1,7 @@
 ﻿Office.actions.associate("buttonFunction", function (event) {
     console.log('Hey, you just pressed a button in Excel ribbon. Test')
     console.log(accessToken)
-    console.log('a1')
+    console.log('a1aa')
 
     event.completed();
 })
