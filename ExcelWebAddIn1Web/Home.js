743bbb4d3397aa6e1c4651b3b56ab2cb26df01d3
Seamus Lu <Seamus.Lu@kbr.com>
2023-12-19 14:35:50 +1100

    a

--- ExcelWebAddIn1Web/Home.js
+++ ExcelWebAddIn1Web/Home.js
@@ -15,7 +15,7 @@
     // Constants for client ID, redirect URL, and resource domain for authentication
     const clientId = "be63874f-f40e-433a-9f35-46afa1aef385"
     const redirectUrl = "https://seamus77x.github.io/index.html"
-    const resourceDomain = "https://gsis-pmo-australia-sensei-dev.crm6.dynamics.com/"
+    const resourceDomain = "https://gsis-pmo-australia-sensei-demo.crm6.dynamics.com/"
 
     // Initialization function that runs each time a new page is loaded.
     Office.initialize = function (reason) {
